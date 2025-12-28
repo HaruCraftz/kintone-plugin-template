a13bf02fee9471a6ca25b76e590ef7410beb4039
shms07 <sunohara.ms@gmail.com>
2025-12-29 03:09:35 +0900

    fix: 不要コメント削除

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -3,13 +3,13 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#3498db', // kintoneのブランドカラーに近い青
+      main: '#3498db', // kintoneカラー
     },
     background: {
       default: '#f5f5f5',
     },
   },
   typography: {
-    fontSize: 14, // kintoneの標準フォントサイズに合わせる
+    fontSize: 14,
   },
 });
