d93772604ba50b39a6e8e75b55d15e08f291d2e5
git-sys-reno <git-sys-reno@comture.com>
2026-01-08 17:36:23 +0900

    fix: 型定義更新に伴い修正

--- src/shared/hooks/useAppFields.ts
+++ src/shared/hooks/useAppFields.ts
@@ -1,46 +1,13 @@
 import { useAtomValue } from 'jotai';
 import { appFieldsAtom } from '@/config/states/kintone';
-import type { KintoneFieldType } from '@/types/kintone';
-
-const fieldTypeMap: Record<string, KintoneFieldType> = {
-  recordNumber: 'RECORD_NUMBER',
-  id: '__ID__',
-  revision: '__REVISION__',
-  creator: 'CREATOR',
-  createdTime: 'CREATED_TIME',
-  modifier: 'MODIFIER',
-  updatedTime: 'UPDATED_TIME',
-  singleLineText: 'SINGLE_LINE_TEXT',
-  multiLineText: 'MULTI_LINE_TEXT',
-  richText: 'RICH_TEXT',
-  number: 'NUMBER',
-  calc: 'CALC',
-  checkBox: 'CHECK_BOX',
-  radioButton: 'RADIO_BUTTON',
-  multiSelect: 'MULTI_SELECT',
-  dropDown: 'DROP_DOWN',
-  userSelect: 'USER_SELECT',
-  organizationSelect: 'ORGANIZATION_SELECT',
-  groupSelect: 'GROUP_SELECT',
-  date: 'DATE',
-  time: 'TIME',
-  dateTime: 'DATETIME',
-  link: 'LINK',
-  file: 'FILE',
-  subtable: 'SUBTABLE',
-  category: 'CATEGORY',
-  status: 'STATUS',
-  statusAssignee: 'STATUS_ASSIGNEE',
-};
-type FieldTypeKeyword = keyof typeof fieldTypeMap;
+import type { FieldType } from '@/types/kintone';
 
 /**
  * kintoneのフィールド一覧を取得するフック
  * すべてのフィールド、または指定した型でフィルタリングしたリストを返却する
  */
-export const useAppFields = (filterTypes?: FieldTypeKeyword[]) => {
+export const useAppFields = (filterTypes?: FieldType[]) => {
   const allFields = useAtomValue(appFieldsAtom);
   const fields = filterTypes ? allFields.filter((f) => filterTypes.includes(f.type)) : allFields;
-  const options = fields.map((f) => ({ label: f.label, code: f.code }));
-  return { fields, options };
+  return { fields };
 };
