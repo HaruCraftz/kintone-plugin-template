d60c15416781dce4b87301aa168d7f09aeceec5b
git-sys-reno <git-sys-reno@comture.com>
2026-01-09 11:18:01 +0900

    feat: ジェネリクス対応

--- src/config/hooks/useDuplicateCheck.ts
+++ src/config/hooks/useDuplicateCheck.ts
@@ -1,14 +1,14 @@
 import { useCallback } from 'react';
-import { useFormContext, useWatch } from 'react-hook-form';
+import { useFormContext, useWatch, type Path } from 'react-hook-form';
 import type { PluginConfig, PluginCondition } from '@/shared/config';
 
 /**
  * 配列内のフィールド重複チェック用フック
  * @param index 現在の行番号
- * @param arrayKey 監視対象の配列フィールド名（デフォルト: 'conditions'）
+ * @param arrayKey 監視対象の配列フィールド名
  */
-export const useDuplicateCheck = (index: number, arrayKey: keyof PluginConfig = 'conditions') => {
-  const { control } = useFormContext<PluginConfig>();
+export const useDuplicateCheck = <T extends PluginConfig>(index: number, arrayKey: Path<T>) => {
+  const { control } = useFormContext<T>();
 
   // 入力値を監視
   const items = useWatch({ control, name: arrayKey });
